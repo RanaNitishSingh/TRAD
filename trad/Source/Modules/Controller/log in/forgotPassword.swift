e9877c021c1581c3c6d009911b765dd6a9eac5fb
RanaNitishSingh <nitish.zeroit@gmail.com>
2022-06-08 12:16:32 +0530

    set the button and pop view

--- trad/Source/Modules/Controller/log in/forgotPassword.swift
+++ trad/Source/Modules/Controller/log in/forgotPassword.swift
@@ -20,6 +20,8 @@ class forgotPassword: UIViewController, UITextFieldDelegate {
 
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(sender:)), name: UIResponder.keyboardWillHideNotification, object: nil);
         self.addBackBtn(textColor: UIColor.black, title: "")
+        self.sendBtn.layer.cornerRadius = 24
+        
     }
     
     @objc func hideKeyboardWhenTappedAround() {
@@ -40,16 +42,16 @@ class forgotPassword: UIViewController, UITextFieldDelegate {
     }
     
     @IBAction func sendAction(_ sender: Any) {
-        
-        let auth = Auth.auth()         
+        let auth = Auth.auth()
         auth.sendPasswordReset(withEmail: emailTxtField.text!) { (error) in
             if let error = error {
                 TRADSingleton.sharedInstance.showAlert(title: "TRAD", msg: "Enter Valid Email".localizedStr(), VC: self, cancel_action: false)
-            }
-            
+            }else{
             TRADSingleton.sharedInstance.showAlert(title: "TRAD", msg: "A password reset email has been sent".localizedStr(), VC: self, cancel_action: false)
-            self.navigationController?.popToRootViewController(animated: true)
-       
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.9) {
+                    self.navigationController?.popToRootViewController(animated: true)
+            }
+            }
         }
     }
 
